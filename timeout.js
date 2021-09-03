98205d95f3e4b793d2b715676345d9783c4a1158
Shahin-Soft-Eng <smshahinmahmud96@gmail.com>
2021-09-04 03:37:35 +0600

    first commit

---
+++ timeout.js
@@ -0,0 +1,17 @@
+function timeImportent  () {
+    console.log('Eivabe cholle javaScript amak vortha banaye choel jabe');
+}
+console.log('First: Ami khub perai ashe');
+console.log('Second: JavaScript er kichui bujhi na');
+// timeImportent();
+// setTimeout(timeImportent,5000); Its importent
+setTimeout(function(){
+    console.log('I am using vs code')
+},5000);
+
+setTimeout(() =>{
+    console.log('Exploring MDN Articles')
+},4000);
+
+console.log('Third:Sob Kichu Mathar upore diya jai');
+console.log('Fourth: Kichui Mathay thake na');
